031e4587cbb08fa12ebbb2ae475734fbb99c641a
Ohiorenua <50307290+Ohior@users.noreply.github.com>
2022-07-08 03:52:59 +0100

    updated depreciated module and added requiremant.txt

--- pastebin/urls.py
+++ pastebin/urls.py
@@ -1,14 +1,15 @@
 from django.contrib import admin
-from django.conf.urls import url, include
+from django.urls import include, path
+
 from posts import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from posts import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^posts/', include('posts.urls')),
-    url(r'^accounts/', include('accounts.urls')),
-    url(r'^$', views.create, name="home"),
+    path(r'^admin/', admin.site.urls),
+    path(r'^posts/', include('posts.urls')),
+    path(r'^accounts/', include('accounts.urls')),
+    path(r'^$', views.create, name="home"),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
--- posts/urls.py
+++ posts/urls.py
@@ -1,16 +1,17 @@
-from django.conf.urls import url
+from django.urls import path
+
 from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 app_name = 'posts'
 
 urlpatterns = [
-    url(r'^$', views.create, name="post_create"),
-    url(r'^loggedin_create$', views.login_create, name="post_login_create"),
-    url(r'^(?P<url>[\w-]+)/$', views.paste_disp, name="disp"),
-    url(r'^loggedin/(?P<url>[\w-]+)/$', views.login_paste_disp, name="login_disp"),
-    url(r'^edit/(?P<id>[\w-]+)/$', views.edit, name="edit"),
-    url(r'^delete/(?P<id>[\w-]+)/$', views.delete, name="delete"),
+    path(r'^$', views.create, name="post_create"),
+    path(r'^loggedin_create$', views.login_create, name="post_login_create"),
+    path(r'^(?P<url>[\w-]+)/$', views.paste_disp, name="disp"),
+    path(r'^loggedin/(?P<url>[\w-]+)/$', views.login_paste_disp, name="login_disp"),
+    path(r'^edit/(?P<id>[\w-]+)/$', views.edit, name="edit"),
+    path(r'^delete/(?P<id>[\w-]+)/$', views.delete, name="delete"),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
